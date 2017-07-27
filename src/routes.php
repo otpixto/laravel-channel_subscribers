db653c7889de3c3536fc7cab3ecf4b5da8da735a
otpixto <otpixto@yandex.ru>
2017-07-27 15:40:56 +0300

    Delete routes.php

--- src/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('add/{a}/{b}', 'Otpixto\ChannelsSubscribers\ChannelsSubscribersController@add');
-Route::get('subtract/{a}/{b}', 'Otpixto\ChannelsSubscribers\ChannelsSubscribersController@subtract');
